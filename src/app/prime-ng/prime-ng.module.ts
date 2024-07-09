31a81749b0f97fc7202c0fc460e63096824f6a43
Leo Ss <leo.vm.cba@gmail.com>
2024-07-09 11:09:47 -0300

    add: primeNG Menu component

---
+++ src/app/prime-ng/prime-ng.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { MenuModule } from 'primeng/menu';
+
+@NgModule({
+  declarations: [],
+  exports: [MenuModule],
+})
+export class PrimeNgModule {}
---
+++ src/app/shared/components/menu/menu.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+  selector: 'shared-menu',
+  templateUrl: './menu.component.html',
+  styles: ``,
+})
+export class MenuComponent implements OnInit {
+  public menuItems: MenuItem[] = [];
+
+  ngOnInit() {
+    this.menuItems = [
+      { label: 'New', icon: 'pi pi-plus' },
+      { label: 'Search', icon: 'pi pi-search' },
+    ];
+  }
+}
