acb4a2692d77689df2271a2a23a62fc36df292ba
Leo Ss <leo.vm.cba@gmail.com>
2024-07-10 16:22:34 -0300

    add: basic page content

--- src/app/products/pages/basics-page/basics-page.component.ts
+++ src/app/products/pages/basics-page/basics-page.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-basics-page',
   templateUrl: './basics-page.component.html',
-  styleUrl: './basics-page.component.css'
+  styleUrl: './basics-page.component.css',
 })
 export class BasicsPageComponent {
+  public textLower: string = 'prueba';
+  public textUpper: string = 'PRUEBA';
+  public textRare: string = 'PrUeBa';
+  templateExpression: string = '';
 
+  setTemplateExpression(textType: string, pipeType: string) {
+    return `{{ ${textType} | ${pipeType} }}`;
+  }
 }
