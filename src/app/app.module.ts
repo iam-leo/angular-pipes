17ef0c2bf6576dadeaef5814a8f82dc531c7c9d1
Leo Ss <leo.vm.cba@gmail.com>
2024-08-27 20:01:18 -0300

    -add: change global language

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -7,6 +7,14 @@ import { AppComponent } from './app.component';
 
 import { SharedModule } from './shared/shared.module';
 
+// Configuracion del locale de la app
+import localeEsAr from '@angular/common/locales/es-AR';
+import localeJa from '@angular/common/locales/ja';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData( localeEsAr );
+registerLocaleData( localeJa );
+
 @NgModule({
   declarations: [AppComponent],
   imports: [
@@ -14,8 +22,12 @@ import { SharedModule } from './shared/shared.module';
     AppRoutingModule,
     SharedModule,
     BrowserAnimationsModule,
+
   ],
-  providers: [],
+  providers: [
+    // Configurar el idioma por defecto de la app
+    { provide: LOCALE_ID, useValue: 'es-AR' },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
