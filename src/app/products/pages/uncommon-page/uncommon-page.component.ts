c3ee34311dd2df5262a69546c90e2f0e2b95baad
Leo Ss <leo.vm.cba@gmail.com>
2024-08-30 17:11:29 -0300

    add: i18n select pipe

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class UncommonPageComponent {
 
+  // i18n Select
+  public name: string = 'Leonel';
+  public gender: 'male' | 'female' = 'male';
+  public invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla'
+  }
+
+
+  changeClient():void{
+    this.name = 'Daniela';
+    this.gender = 'female';
+  }
 }
